12c1d03ca5584a273fded160dab4a90e207bded1
Travinkel <ankersoe@protonmail.com>
2025-09-27 13:14:57 +0200

    test(genre): fix CreateGenre_WritesRow test to assert against GenreDTO.Id

--- tests/LibraryProject.Tests/GenreServiceTest.cs
+++ tests/LibraryProject.Tests/GenreServiceTest.cs
@@ -1,6 +1,18 @@
-﻿namespace LibraryProject.Tests;
+﻿using LibraryProject.Api.DTOs;
+using LibraryProject.Api.Services;
+using Microsoft.EntityFrameworkCore;
 
-public class GenreServiceTest
+namespace LibraryProject.Tests;
+
+public class GenreServiceTests : ServiceTestBase<GenreService>
 {
-    
+    [Fact]
+    public async Task CreateGenre_WritesRow()
+    {
+        var dto = await Svc.CreateAsync(new CreateGenreDTO("Magi"));
+        var found = await Db.Genres.AsNoTracking().SingleAsync(g => g.Id == dto.Id);
+
+        Assert.Equal("Magi", found.Name);
+        Assert.True(found.CreatedAt.HasValue);
+    }
 }
